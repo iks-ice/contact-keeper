daaeeb8d9065460ce33ca2e031c631e2b7c8497e
Oleg Parshyn <olegparshyn@MacBook-Pro-LoopMe-3.local>
2020-01-21 12:57:47 +0200

    added first user

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
 
@@ -13,20 +14,34 @@ router.post('/', [
     check('name', 'Name is required').notEmpty({ignore_whitespace: true}),
     check('email', 'Valid e-mail is required').isEmail(),
     check('password', 'Password should consist of 4 or more characters').isLength({min: 4})
-], (req, res) => {
+], async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(422).json({errors: errors.array()});
     }
 
-    // User.create({
-    //     name: req.body.name,
-    //     email: '',
-    //     password: '',
-    //     date: Date.now()
-    // }).then(user => res.json(user));
-    res.send(req.body);
+    const {name, password, email} = req.body;
+
+    try {
+        let user = await User.findOne({email});
+        if (user) {
+            return res.status(400).json({msg: 'User already exists'})
+        }
+
+        user = new User({name, password, email});
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User saved')
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error');
+    }
 });
 
 
